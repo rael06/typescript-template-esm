3fcfd7cf1839fe6f4c53ee7bd9bcf52af7a14fc1
Rael Calitro <rael06@hotmail.fr>
2024-03-27 19:35:38 +0100

    update Fetcher

--- src/Services/Fetcher.ts
+++ src/Services/Fetcher.ts
@@ -1,13 +1,11 @@
 import { singleton } from "tsyringe";
-
-const fetch = import("node-fetch");
-type Fetch = Awaited<typeof fetch>;
+import fetch from "node-fetch";
 
 @singleton()
 export default class Fetcher {
   public async fetch(
-    ...params: Parameters<Fetch["default"]>
-  ): ReturnType<Fetch["default"]> {
-    return (await fetch).default(params[0], params[1]);
+    ...params: Parameters<typeof fetch>
+  ): ReturnType<typeof fetch> {
+    return await fetch(params[0], params[1]);
   }
 }
